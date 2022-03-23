3fb2fe5485adec07e5b629189741c55a00937428
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-03-23 21:45:21 +0000

    kite has them areas :)

---
+++ kite_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Huzaifa Khalid
+# Created on: March 2020
+# This program calcualtes the perimeter of a kite 
+#with user input
+
+
+def main():
+    # this function shows formatting output
+    print("This program will calcualte the perimeter of a kite")
+    
+    # input
+    print("")
+    side_a = int(input("Enter the value for side a (cm): "))
+    side_b = int(input("Enter the value for side b (cm): "))
+    
+    # process
+    perimeter = 2 * (side_a + side_b)
+    
+    # output
+    print("")
+    print("Therefore the perimeter is {} cm ".format(perimeter))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
