5bb56b43e4641b48a7eed8b625f3d35567ebb483
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-03-23 21:49:01 +0000

    ok now we're done :)

--- kite_perimeter.py
+++ kite_perimeter.py
@@ -2,22 +2,22 @@
 
 # Created by: Huzaifa Khalid
 # Created on: March 2020
-# This program calcualtes the perimeter of a kite 
-#with user input
+# This program calcualtes the perimeter of a kite
+# with user input
 
 
 def main():
     # this function calculates the perimeter
     print("This program will calcualte the perimeter of a kite")
-    
+
     # input
     print("")
     side_a = int(input("Enter the value for side a (cm): "))
     side_b = int(input("Enter the value for side b (cm): "))
-    
+
     # process
     perimeter = 2 * (side_a + side_b)
-    
+
     # output
     print("")
     print("Therefore the perimeter is {} cm ".format(perimeter))
